1f9904267ad0a94f70ddb245e5b8538b286d40da
glimadev <gilvanjunior360@gmail.com>
2017-05-05 14:21:06 -0300

    separate helpers and SpellingCorrector class from Program class

---
+++ SpellingCorrector/Helpers.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpellingCorrector
+{
+    public static class Helpers
+    {
+        public static Dictionary<string, string> Splits(this string word)
+        {
+            var splits = new Dictionary<string, string>();
+
+            for (int i = 0; i < word.Length + 1; i++)
+            {
+                splits.Add(word.Substring(0, i), word.Substring(i));
+            }
+
+            return splits;
+        }
+
+        public static string[] Deletes(this Dictionary<string, string> splits)
+        {
+            return splits.Where(r => r.Value != "")
+                .Select(x => x.Key + x.Value.Substring(x.Value.Length - (x.Value.Length - 1))).ToArray();
+        }
+
+        public static string[] Transposes(this Dictionary<string, string> splits)
+        {
+            return splits.Where(r => r.Value.Length > 1)
+                .Select(x => x.Key + x.Value.Substring(1, 1) + x.Value.Substring(0, 1) + x.Value.Substring(2, x.Value.Length - 2)).ToArray();
+        }
+
+        public static string[] Replaces(this Dictionary<string, string> splits, char[] letters)
+        {
+            string[] replaces = new string[letters.Count() * (splits.Count - 1)];
+
+            int replaceAux = 0;
+
+            foreach (var item in splits)
+            {
+                if (item.Value != "")
+                {
+                    foreach (var letter in letters)
+                    {
+                        replaces[replaceAux] = item.Key + letter + item.Value.Substring(1, item.Value.Length - 1);
+
+                        replaceAux++;
+                    }
+                }
+            }
+
+            return replaces;
+        }
+
+        public static string[] Inserts(this Dictionary<string, string> splits, char[] letters)
+        {
+            string[] inserts = new string[letters.Count() * splits.Count];
+
+            int insertAux = 0;
+
+            foreach (var item in splits)
+            {
+                foreach (var letter in letters)
+                {
+                    inserts[insertAux] = item.Key + letter + item.Value;
+
+                    insertAux++;
+                }
+            }
+
+            return inserts;
+        }
+
+        public static string Max(this IEnumerable<string> source, Func<string, double> selector)
+        {
+            double max = 0;
+
+            string itemMax = "";
+
+            foreach (var item in source)
+            {
+                double maxAux = selector.Invoke(item);
+
+                if (maxAux >= max)
+                {
+                    max = maxAux;
+
+                    itemMax = item;
+                }
+            }
+
+            return itemMax;
+        }
+    }
+}
---
+++ SpellingCorrector/SpellingCorrector.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SpellingCorrector
+{
+    public class SpellingCorrector
+    {
+        private static Dictionary<string, int> _words = new Dictionary<string, int>();
+
+        private static double _wordsSum = 0;
+
+        private static string[] _candidates = null;
+
+        private static char[] _letters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        public SpellingCorrector()
+        {
+            if (_words.Count == 0)
+            {
+                _words = File.ReadAllText(@"c:\temp\text.txt")
+                           .Split('\n')
+                           .SelectMany(x => x.Split(' '))
+                           .Select(x => Regex.Replace(x.Trim().ToLower(), @"[^a-z]+", String.Empty))
+                           .Where(x => !string.IsNullOrEmpty(x))
+                           .GroupBy(x => x)
+                           .OrderBy(x => x.Key)
+                           .ToDictionary(x => x.Key, x => x.Count());
+
+                _wordsSum = _words.Sum(x => x.Value);
+            }
+        }
+
+        public double ProbabilityOfWord(string word)
+        {
+            return (_words.Any(x => x.Key == word)) ? (_words[word] / _wordsSum) : 0;
+        }
+
+        public string Correction(string word)
+        {
+            var watch = Stopwatch.StartNew();
+
+            _candidates = Candidates(Regex.Replace(word.Trim().ToLower(), @"[^a-z]+", String.Empty));
+
+            watch.Stop();
+
+            Console.WriteLine((watch.Elapsed.TotalMilliseconds).ToString("0.00 ms"));
+
+            return _candidates.Max(ProbabilityOfWord);
+        }
+
+        public string GetCandidates()
+        {
+            return string.Join(", ", _candidates.OrderByDescending(x => ProbabilityOfWord(x)));
+        }
+
+        private string[] Candidates(string word)
+        {
+            string[] candidates = Known(new string[] { word });
+
+            if (candidates.Count > 0) return candidates;
+
+            candidates = Known(Edits1(word));
+
+            if (candidates.Count() > 0) return candidates;
+
+            candidates = Known(Edits2(word));
+
+            if (candidates.Count() > 0) return candidates;
+
+            return new string[] { word };
+        }
+
+        private string[] Edits2(string word)
+        {
+            var edit1 = Edits1(word);
+
+            List<string> edit2 = new List<string>();
+
+            foreach (var item in edit1) edit2.AddRange(Edits1(item));
+
+            return edit2.GroupBy(x => x).Select(x => x.Key).ToArray();
+        }
+
+        private string[] Edits1(string word)
+        {
+            var splits = word.Splits();
+
+            return splits.Inserts(_letters).Union(splits.Deletes()).Union(splits.Transposes()).Union(splits.Replaces(_letters)).ToArray();
+        }
+
+        private string[] Known(string[] v)
+        {
+            List<string> item2 = new List<string>();
+
+            List<string> wordsList = _words.Select(x => x.Key).ToList();
+
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (wordsList.BinarySearch(v[i]) >= 0)
+                {
+                    item2.Add(v[i]);
+                }
+            }
+
+            return item2.ToArray();
+        }
+    }
+}
